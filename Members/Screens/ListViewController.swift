4c59ff459d28b63b8ede830626837ef399d4bd85
Ufuk Canlı <iletisim@ufukcanli.com>
2021-03-26 20:22:46 +0300

    Fetch members.

--- Members/Screens/ListViewController.swift
+++ Members/Screens/ListViewController.swift
@@ -8,8 +8,20 @@
 import UIKit
 
 class ListViewController: UITableViewController {
-
+    
+    private var company = Bundle.main.decode(Company.self, from: "hipo.json", keyDecodingStrategy: .convertFromSnakeCase)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return company.members.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "MemberCell")
+        cell.textLabel?.text = String(company.members[indexPath.row].name)
+        return cell
     }
 }
