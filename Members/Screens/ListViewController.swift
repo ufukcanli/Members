61347c9b9233bacc37930f6e47e54051d3869288
Ufuk Canlı <iletisim@ufukcanli.com>
2021-03-26 19:42:13 +0300

    Add table view controller.

---
+++ Members/Screens/ListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ListViewController.swift
+//  Members
+//
+//  Created by Ufuk Canlı on 26.03.2021.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
