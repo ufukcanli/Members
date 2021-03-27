5d3f7c8f3d63f6dec8f69e86a12a78a17b074139
Ufuk Canlı <iletisim@ufukcanli.com>
2021-03-27 14:10:26 +0300

    Add buttons.

--- Members/Screens/ListViewController.swift
+++ Members/Screens/ListViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
+    private lazy var sortButton = UCButton(backgroundColor: .black, title: "Sort Members")
+    private lazy var addButton = UCButton(backgroundColor: .systemGreen, title: "Add New Member")
+    private lazy var buttonStackView = UIStackView(arrangedSubviews: [sortButton, addButton])
+    
     private var company = Bundle.main.decode(Company.self, from: "hipo.json", keyDecodingStrategy: .convertFromSnakeCase)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configure()
+    }
+    
+    @objc private func didTapSortButton() {
+        sortButton.startAnimatingPressActions()
+    }
+    
+    @objc private func didTapAddButton() {
+        addButton.startAnimatingPressActions()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,8 +40,25 @@ class ListViewController: UITableViewController {
     }
     
     private func configure() {
+        let padding: CGFloat = 30
+        
         navigationItem.title = "Members"
         navigationController?.navigationBar.prefersLargeTitles = true
         tableView.tableFooterView = UIView(frame: .zero)
+        
+        addButton.addTarget(self, action: #selector(didTapAddButton), for: .touchUpInside)
+        sortButton.addTarget(self, action: #selector(didTapSortButton), for: .touchUpInside)
+        
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.alignment = .center
+        buttonStackView.axis = .vertical
+        buttonStackView.spacing = 15
+        view.addSubview(buttonStackView)
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            buttonStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding)
+        ])
     }
 }
---
+++ Members/Views/UCButton.swift
@@ -0,0 +1,45 @@
+//
+//  UCButton.swift
+//  Members
+//
+//  Created by Ufuk Canlı on 27.03.2021.
+//
+
+import UIKit
+
+class UCButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(backgroundColor: UIColor, title: String) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title.uppercased(), for: .normal)
+    }
+    
+    private func configure() {
+        clipsToBounds = true
+        layer.masksToBounds = false
+        layer.cornerRadius = 25
+        layer.shadowRadius = 7
+        layer.shadowOpacity = 0.6
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowColor = UIColor.darkGray.cgColor
+        
+        setTitleColor(.white, for: .normal)
+        setTitleColor(UIColor(white: 1, alpha: 0.3), for: .highlighted)
+        titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: 250).isActive = true
+        heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+}
